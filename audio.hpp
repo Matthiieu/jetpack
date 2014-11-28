34dcf428f019d7526d74775b358c51b73667b7e4
Matthiiieu <matthieu.blngr@gmail.com>
2014-11-29 00:08:39 +0100

    Ajouté fichier audio

---
+++ audio.hpp
@@ -0,0 +1,43 @@
+#ifndef AUDIO_HPP
+#define AUDIO_HPP
+
+#include <SFML/Audio.hpp>
+
+class Audio{
+	public:
+	Audio () {
+		music2fond_ = new sf::Music;
+		music2menu_ = new sf::Music;
+		music2fond_->openFromFile("Joyrade.ogg");
+		music2menu_->openFromFile("Joyrade_menu.ogg");
+	}
+
+	~Audio(){
+		delete music2fond_;
+		delete music2menu_;
+	}
+
+	void play_fond(void){
+		music2fond_->play();
+		music2fond_->setVolume(100);
+	}
+
+	void stop_fond(void){
+		music2fond_->stop();
+	}
+	
+	void play_menu(void){
+		music2menu_->play();
+		music2menu_->setVolume(100);
+	}
+
+	void stop_menu(void){
+		music2menu_->stop();
+	}
+
+	private:
+		sf:: Music *music2fond_;
+		sf:: Music *music2menu_;
+};
+
+#endif
